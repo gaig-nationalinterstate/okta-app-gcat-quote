755973d8232111cb32fe3b7049dc0cbd5284f760
Matt Everhart <matt.everhart@natl.com>
2023-05-17 09:00:52 -0400

    Added auth_server_policy.tf

---
+++ auth_server_policy.tf
@@ -0,0 +1,19 @@
+resource "okta_auth_server_policy" "gcat-quote-policy" {
+  auth_server_id   = okta_auth_server.gcat-quote-auth-server.id
+  client_whitelist = [okta_app_oauth.gcat-quote.id]
+  description      = "GCAT Quote Access Policy"
+  name             = "GCAT Quote Access Policy"
+  priority         = "1"
+  status           = "ACTIVE"
+}
+
+# Policy Rule
+resource "okta_auth_server_policy_rule" "gcat-quote-rule" {
+  auth_server_id       = okta_auth_server.gcat-quote-auth-server.id
+  policy_id            = okta_auth_server_policy.gcat-quote-policy.id
+  status               = "ACTIVE"
+  name                 = "GCAT Quote Default Policy Rule"
+  priority             = 1
+  grant_type_whitelist = ["authorization_code", "implicit"]
+  scope_whitelist      = "*"
+}
